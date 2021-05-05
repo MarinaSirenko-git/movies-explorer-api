fdc7b681896b0fdacd50a5d6ccf68d80ab90ae49
Marina Sirenko <sirenko.workspace@gmail.com>
2021-05-05 20:37:44 +0300

    инициализировать package.json, добавить инфраструктурные файлы .gitignore .editorconfig .eslint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+  ],
+  rules: {
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+    'linebreak-style': ['error', 'unix'],
+  },
+};
