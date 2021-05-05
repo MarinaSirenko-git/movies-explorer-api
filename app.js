1b8a16b95c59c68db2ae68abaad5bd3271f25834
Marina Sirenko <sirenko.workspace@gmail.com>
2021-05-05 21:08:13 +0300

    развернуть сервер с помощью express

---
+++ app.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+const express = require('express');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
