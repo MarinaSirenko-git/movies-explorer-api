ec7d81c305398aba1875a3335a8f663dcb80dc96
Marina Sirenko <sirenko.workspace@gmail.com>
2021-05-06 09:57:34 +0300

    реализовать подключение к mongo из app.js

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 /* eslint-disable no-console */
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
+const { DB_CONN = 'mongodb://localhost:27017/moviesdb' } = process.env;
 
 const app = express();
+
+mongoose.connect(DB_CONN, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
